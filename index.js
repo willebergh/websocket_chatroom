fbbb68b45d5d3ea3ebc536207958f2cd8eb29590
willebergh <willebergh@outlook.com>
2019-07-19 16:08:34 +0200

    added router to main file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(require("./routes"))
+
 const PORT = 3000 | process.env.PORT;
 app.listen(PORT, () => {
     console.log(`server started on port: ${PORT}.`)
